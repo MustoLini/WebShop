0d604781b6f943d548c3453f4f3decfae5f661c1
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-03-27 14:25:35 +0200

    Updating name To correct and adding some small logic

--- src/main/java/com/example/webshop/data/GoodsRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.webshop.data;
-
-import com.example.webshop.business.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GoodsRepository extends JpaRepository<Product,Long> {
-
-}
