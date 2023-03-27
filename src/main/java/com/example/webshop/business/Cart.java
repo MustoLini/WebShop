84ef48bf42c50600d515f3f48a3846b8ec53b749
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-03-27 16:22:59 +0200

    Changed cart from a entity to a normal class that holds a list with CartItems

--- src/main/java/com/example/webshop/business/Cart.java
+++ src/main/java/com/example/webshop/business/Cart.java
@@ -2,23 +2,15 @@ package com.example.webshop.business;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Table(name = "cart")
+
 public class Cart {
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @Column(name = "id", nullable = false)
-    private Long id;
-    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
-    private List<Product> productList;
 
-    public Long getId() {
-        return id;
-    }
+    List<CartItem> cartItems;
 
-    public void setId(Long id) {
-        this.id = id;
+    public Cart() {
+        this.cartItems = new ArrayList<>();
     }
 }
