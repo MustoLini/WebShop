b4a848ffe5dbf7365e3e28afb9dd8cff8011dcb2
Johannes <danielssonjohannes@gmail.com>
2023-03-29 15:55:49 +0200

    Updating PersonRepository

---
+++ src/main/java/com/example/webshop/data/PersonRepository.java
@@ -0,0 +1,13 @@
+package com.example.webshop.data;
+
+import com.example.webshop.business.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PersonRepository extends JpaRepository<Person,Long> {
+    Optional<Person>findByEmail(String email);
+    List<Person> findByEmailAndPassWord(String email, String password);
+
+}
