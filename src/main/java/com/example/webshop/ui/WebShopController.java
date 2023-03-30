59037c090b1900dd97fc2c274f107ed5433217fc
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-03-30 13:44:32 +0200

    Adding Get mapping and post mapping and so forth

--- src/main/java/com/example/webshop/ui/WebShopController.java
+++ src/main/java/com/example/webshop/ui/WebShopController.java
@@ -1,5 +1,6 @@
 package com.example.webshop.ui;
 
+import com.example.webshop.business.Product;
 import com.example.webshop.business.WebsiteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -15,7 +16,7 @@ public class WebShopController {
     @PostMapping("/login")
     public String login(@RequestParam String loginUser, @RequestParam String password, Model model){
         model.addAttribute("person", websiteService.login(loginUser,password));
-        return "redirect:/product";
+        return "redirect:/productShop";
     }
     @GetMapping("/register")
     public String registerWebsite(Model model){
@@ -25,6 +26,28 @@ public class WebShopController {
     public String register(@RequestParam String loginUser, @RequestParam String password, Model model){
         String checker= websiteService.checkIfUserExist(loginUser, password);
         model.addAttribute("checker", checker);
-        return "redirect:/index.html";
+        return "register";
     }
+    @GetMapping("/addingProducts")
+    public String addProductWebsite(){
+        return "addingProducts";
+    }
+    @PostMapping("/addingProducts")
+    public String addProduct(@RequestParam String productName, @RequestParam Double price, @RequestParam String category, Model model){
+        model.addAttribute("product", websiteService.addProduct(productName,price,category));
+        return "addingProducts";
+    }
+    @PostMapping ("/productShop")
+    public String showAllProducts(Model model){
+        model.addAttribute("productShop",websiteService.getAllProducts());
+        return "productShop";
+    }
+    @GetMapping("/product")
+    public String buyProduct(@RequestParam long id, Model model){
+        Product product=websiteService.getByIdProduct(id);
+        model.addAttribute("product",product);
+        return "/";
+    }
+
+
 }
