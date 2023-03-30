f73c041870187d42cef994e38026a4ab7d8382e6
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-03-30 19:13:17 +0200

    Adding getters and setters to CartItem for productShop.html

--- src/main/java/com/example/webshop/business/CartItem.java
+++ src/main/java/com/example/webshop/business/CartItem.java
@@ -24,6 +24,22 @@ public class CartItem {
     public CartItem() {
     }
 
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
     public CartItem(Product product, int amount) {
         this.product = product;
         this.amount = amount;
