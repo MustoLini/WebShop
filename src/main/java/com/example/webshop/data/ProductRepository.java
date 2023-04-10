62618eef1750d78a3ca32617c7860930e69f897b
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-04-10 12:21:56 +0200

    Refactoring by the correct format

--- src/main/java/com/example/webshop/data/ProductRepository.java
+++ src/main/java/com/example/webshop/data/ProductRepository.java
@@ -4,13 +4,11 @@ import com.example.webshop.business.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Optional;
 
-public interface ProductRepository extends JpaRepository<Product,Long> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findByName(String name);
 
     List<Product> findByCategory(String category);
-
 
 
 }
