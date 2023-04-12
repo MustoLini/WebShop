0808cfd5d2f732c94cb4c75917815f0db7986843
Johannes Danielsson <danielssonjohannes@gmail.com>
2023-04-12 14:18:38 +0200

    trying out

--- src/main/java/com/example/webshop/data/OrderRepository.java
+++ src/main/java/com/example/webshop/data/OrderRepository.java
@@ -3,6 +3,7 @@ package com.example.webshop.data;
 import com.example.webshop.business.CustomerOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrderRepository extends JpaRepository<CustomerOrder, Long> {
+import java.util.List;
 
+public interface OrderRepository extends JpaRepository<CustomerOrder, Long> {
 }
